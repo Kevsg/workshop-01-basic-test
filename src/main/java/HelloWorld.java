2f4ab1fe837554b64e3af868bba379230db66cd3
Phongpak Taothong <phongpaktao@mathregit.group>
2020-08-03 11:47:26 +0700

    Add uppercase

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     }
 
     public String greeting(String name) {
-        return "Hello " + name;
+        String firstLetter = name.substring(0, 1).toUpperCase();
+        return "Hello " + firstLetter + name.substring(1, name.length());
     }
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -14,14 +14,14 @@ public class HelloWorldTest {
     @Test
     public void greeting_with_john_should_return_Hello_John() {
         HelloWorld h = new HelloWorld();
-        String s = h.greeting("John");
+        String s = h.greeting("john");
         assertEquals("Hello John",s);
     }
 
     @Test
     public void greeting_with_dan_should_return_Hello_Dan() {
         HelloWorld h = new HelloWorld();
-        String s = h.greeting("Dan");
+        String s = h.greeting("dan");
         assertEquals("Hello Dan",s);
     }
 }
