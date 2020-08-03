e4d56b119c3a35d67a710d229f401a49477ae0aa
Phongpak Taothong <phongpaktao@mathregit.group>
2020-08-03 11:32:53 +0700

    Workshop 01

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public String greeting() {
+        return "Hello World";
+    }
+
+    public String greeting(String name) {
+        return "Hello " + name;
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void greeting_should_return_Hello_World() {
+        HelloWorld h = new HelloWorld();
+        String s = h.greeting();
+        assertEquals("Hello World",s);
+    }
+
+    @Test
+    public void greeting_with_john_should_return_Hello_John() {
+        HelloWorld h = new HelloWorld();
+        String s = h.greeting("John");
+        assertEquals("Hello John",s);
+    }
+
+    @Test
+    public void greeting_with_dan_should_return_Hello_Dan() {
+        HelloWorld h = new HelloWorld();
+        String s = h.greeting("Dan");
+        assertEquals("Hello Dan",s);
+    }
+}
